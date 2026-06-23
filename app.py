ebc2bc85b7c0312b62974decdfeecf0a6a360058
ailo-tk <theresa@ailodata.com>
2026-06-23 13:39:43 +0200

    Update app.py to fix TV screen size (try 6)
    scale: .67

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ device_config = {
     "ipad":      {"scale": 0.75, "height": 900},
     "laptop13":  {"scale": 0.60, "height": 680},
     "laptop15":  {"scale": 0.72, "height": 820},
-    "desktop":   {"scale": 0.70, "height": 950},
-    "1920x1080": {"scale": 0.70, "height": 920},  # New configuration for 1080p
+    "desktop":   {"scale": 0.67, "height": 950},
+    "1920x1080": {"scale": 0.67, "height": 920},  # New configuration for 1080p
 }
 cfg = device_config[DEVICE]
 
